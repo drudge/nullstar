b68eac91c62084e016f2a72be1590ae079ee7692
Nicholas Penree <nick@penree.com>
2014-02-22 20:15:30 -0500

    Storage and binding

---
+++ index.js
@@ -0,0 +1,69 @@
+/*!
+ * Nullstar
+ * Copyright(c) 2014 Nicholas Penree <nick@penree.com>
+ * MIT Licensed
+ */
+
+/**
+ * Module dependencies.
+ */
+
+var replify = require('replify');
+var Bot = require('./lib/bot');
+var Plugin = require('./lib/plugin');
+var Storage = require('./lib/storage');
+
+/**
+ * If we weren't required, start an instance of a nullstar bot.
+ */
+
+if (!module.parent) {
+  var config = require(process.env.CONFIG || './config.json');
+  var bot = new Bot(config);
+  
+  // setup the bot
+  bot
+    .plugin('*')
+    .connect();
+  
+  // setup a repl for fun and profit
+  replify({
+    name: 'nullstar',
+    contexts: {
+      bot: bot,
+      plugins: bot.plugins,
+      storage: bot.storage,
+      irc: bot.connection,
+      db: bot.storage
+    }
+  });
+  
+  // gracefully exit
+  process.on('SIGTERM', function() {
+    bot.storage.close(function() {
+      bot.disconnect('bye - https://twitter.com/' + config['twitter username'], function() {
+        setTimeout(function() {
+          process.exit();
+        }, 2000);
+      });
+    });
+  });
+}
+
+/**
+ * Expose `Bot`.
+ */
+
+exports.Bot = Bot;
+
+/**
+ * Expose `Plugin`.
+ */
+
+exports.Plugin = Plugin;
+
+/**
+ * Expose `Storage`.
+ */
+
+exports.Storage = Storage;
